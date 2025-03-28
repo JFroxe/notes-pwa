7aeb257337a6494280c23e5bb3d2c4652e603ebe
JFroxe <147384936+JFroxe@users.noreply.github.com>
2025-03-27 19:35:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Clientes from "./pages/Clientes";
+import Ordenes from "./pages/Ordenes";
+import Login from "./pages/Login";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/ordenes" element={<Ordenes />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
