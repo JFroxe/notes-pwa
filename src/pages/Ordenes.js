b09ab5f7983595bde52067df9a04a6569da2e2e2
JFroxe <147384936+JFroxe@users.noreply.github.com>
2025-03-27 19:36:19 -0500

    Update Ordenes.js

--- src/pages/Ordenes.js
+++ src/pages/Ordenes.js
@@ -1,1 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { openDB } from "../utils/indexedDB";
+import OrdenForm from "../components/OrdenForm";
 
+function Ordenes() {
+  const [ordenes, setOrdenes] = useState([]);
+  const [clientes, setClientes] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = await openDB();
+
+      const clienteTransaction = db.transaction("Clientes", "readonly");
+      const clienteStore = clienteTransaction.objectStore("Clientes");
+      const clienteRequest = clienteStore.getAll();
+
+      clienteRequest.onsuccess = () => {
+        setClientes(clienteRequest.result);
+      };
+
+      const ordenTransaction = db.transaction("Ordenes", "readonly");
+      const ordenStore = ordenTransaction.objectStore("Ordenes");
+      const ordenRequest = ordenStore.getAll();
+
+      ordenRequest.onsuccess = () => {
+        setOrdenes(ordenRequest.result);
+      };
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Órdenes</h1>
+      <button onClick={() => setShowForm(true)}>Añadir Orden</button>
+      {showForm && <OrdenForm clientes={clientes} />}
+      <ul>
+        {ordenes.map((orden) => (
+          <li key={orden.id}>
+            {orden.dispositivo} - {orden.estado}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Ordenes;
