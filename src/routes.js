1017c1177a8ab2b3a23240c51d8b774489d694f8
JFroxe <147384936+JFroxe@users.noreply.github.com>
2025-03-27 19:42:37 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,11 @@
+import Clientes from "./pages/Clientes";
+import Ordenes from "./pages/Ordenes";
+import Login from "./pages/Login";
 
+const routes = [
+  { path: "/", element: <Login /> },
+  { path: "/clientes", element: <Clientes /> },
+  { path: "/ordenes", element: <Ordenes /> },
+];
+
+export default routes;
